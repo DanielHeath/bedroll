973b4574fe0d6fa9cf230387ef3e521b14802ee3
Daniel Heath <daniel.r.heath@gmail.com>
2012-10-05 15:56:47 +1000

    Fix buildscript

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-bundle exec bonsai repot
+if ! [ -e output/.git ] ; then
+  rm -rf output
+  git clone git@github.com:DanielHeath/bedroll-build.git output
+fi
+bundle exec bonsai --repot
 cd output
 git add -A .
 git commit -m "Build"
