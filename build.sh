a9fd0846bfdabf6427879012ae6b0dc1498b8173
Daniel Heath <daniel.r.heath@gmail.com>
2012-10-05 15:59:50 +1000

    Buildscript seems to finally be working

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-if ! [ -e output/.git ] ; then
-  rm -rf output
-  git clone git@github.com:DanielHeath/bedroll-build.git output
+if ! [ -e output2/.git ] ; then
+  rm -rf output2
+  git clone git@github.com:DanielHeath/bedroll-build.git output2
 fi
+rm -rf output
 bundle exec bonsai --repot
+cp -r output2/.git output/
 cd output
 git add -A .
 git commit -m "Build"
